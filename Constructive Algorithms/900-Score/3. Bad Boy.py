3c17e1a3b9573c17a2b542defc0c7df2d3f786a9
Ruijie Pan <110296888+XiaolanPan@users.noreply.github.com>
2023-06-11 15:20:49 +0800

    Create 3. Bad Boy.py

---
+++ Constructive Algorithms/900-Score/3. Bad Boy.py
@@ -0,0 +1,8 @@
+link : https://codeforces.com/problemset/problem/1537/B
+Code :
+  t = eval(input())
+  while t > 0:
+      n,m,i,j = map(int,input().split())
+      print(1,1,n,m)
+      t -= 1
+    
